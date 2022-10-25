54c2f5878532cc8af9fedd6f50f4db92b06deca4
Venom-61 <subhendukarmakar61@gmail.com>
2022-10-26 02:12:34 +0530

    solutions added

--- d1.cpp
+++ d1.cpp
@@ -5,19 +5,27 @@ using namespace std;
 
 // D1. Balance (Easy Version)
 
-void solve() {
-    int a;
-    cin >> a;
-}
-
-int main() {
+int32_t main() {
     ios::sync_with_stdio(false);
+    
+    int q;
+    cin >> q;
+    map<ll, ll> mex;
+    map<ll, bool> visited;
+    while(q--) {
+        char ch; ll k;
+        cin >> ch >> k;
 
-    int tc = 1;
-    cin >> tc;
-    for(int i = 1; i <= tc; i++) {
-        // cout << "Case: #" << i << " ";
-        solve();
+        if(ch == '+') {
+            visited[k] = true;
+        } else {
+            ll v = mex[k] + k;
+            while(visited[v]) {
+                mex[k] = v;
+                v += k;
+            }
+            cout << v << "\n";
+        }
     }
 
     return 0;
